87569de6b2981d6734afb0115ab5bc58cbbca1cb
Kynan <k.song@blueyonder.co.uk>
2019-04-27 21:36:39 +0100

    Finished get request

--- create.js
+++ create.js
@@ -1,55 +1,78 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
+//import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
-
+export async function main(event, context) {
     const data = JSON.parse(event.body);
-
     const params = {
         TableName: "notes",
-        // 'Item' contains the attributes of the item to be created
-        // - 'userId': user identities are federated through the
-        //             Cognito Identity Pool, we will use the identity id
-        //             as the user id of the authenticated user
-        // - 'noteId': a unique uuid
-        // - 'content': parsed from request body
-        // - 'attachment': parsed from request body
-        // - 'createdAt': current Unix timestamp
-
         Item: {
-            userId: event.requestContext.identity.cognitoIdentityId,
-            noteId: uuid.v1(),
-            content: data.content,
-            attachment: data.attachment,
-            createdAt: Date.now()
+        userId: event.requestContext.identity.cognitoIdentityId,
+        noteId: uuid.v1(),
+        content: data.content,
+        attachment: data.attachment,
+        createdAt: Date.now()
         }
     };
 
-    dynamoDb.put(params, (error, data) => {
-
-        const headers = {
-            // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-            "Access-Control-Allow-Origin" : "*",
-            "Access-Control-Allow-Credentials" : true
-        };
-
-        if (error) {
-            const response = {
-                statusCode: 500,
-                headers: headers,
-                body: JSON.stringify({ status: false})
-            };
-            callback(null, response);
-            return;
-        }
-        // Return status code 200 and the newly created item
-        const response = {
-            statusCode: 200, 
-            headers: headers,
-            body: JSON.stringify(params.Item)
-        };
-        callback(null, response);
-    });
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        return failure({ status: false });
+    }
 }
+
+// const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+// export function main(event, context, callback) {
+
+//     const data = JSON.parse(event.body);
+
+//     const params = {
+//         TableName: "notes",
+//         // 'Item' contains the attributes of the item to be created
+//         // - 'userId': user identities are federated through the
+//         //             Cognito Identity Pool, we will use the identity id
+//         //             as the user id of the authenticated user
+//         // - 'noteId': a unique uuid
+//         // - 'content': parsed from request body
+//         // - 'attachment': parsed from request body
+//         // - 'createdAt': current Unix timestamp
+
+//         Item: {
+//             userId: event.requestContext.identity.cognitoIdentityId,
+//             noteId: uuid.v1(),
+//             content: data.content,
+//             attachment: data.attachment,
+//             createdAt: Date.now()
+//         }
+//     };
+
+//     dynamoDb.put(params, (error, data) => {
+
+//         const headers = {
+//             // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+//             "Access-Control-Allow-Origin" : "*",
+//             "Access-Control-Allow-Credentials" : true
+//         };
+
+//         if (error) {
+//             const response = {
+//                 statusCode: 500,
+//                 headers: headers,
+//                 body: JSON.stringify({ status: false})
+//             };
+//             callback(null, response);
+//             return;
+//         }
+//         // Return status code 200 and the newly created item
+//         const response = {
+//             statusCode: 200, 
+//             headers: headers,
+//             body: JSON.stringify(params.Item)
+//         };
+//         callback(null, response);
+//     });
+// }
